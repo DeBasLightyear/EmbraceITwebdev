5efbbe81fca50dd98c53ad31b47979428601ecae
DeBasLightyear <baskleisen@gmail.com>
2018-04-05 15:56:31 +0200

    Baked to perfection

---
+++ Courseraopdr2/js/script.js
@@ -0,0 +1,17 @@
+
+
+const sausageLeft = document.getElementById("sausageLeft")
+sausageLeft.addEventListener("mouseover", ()=>{
+    sausageLeft.src = "knakworst_hover.png"
+})
+sausageLeft.addEventListener("mouseout", ()=>{
+    sausageLeft.src = "knakworst.png"
+})
+
+const sausageRight = document.getElementById("sausageRight")
+sausageRight.addEventListener("mouseover", ()=>{
+    sausageRight.src = "knakworst_hover.png"
+})
+sausageRight.addEventListener("mouseout", ()=>{
+    sausageRight.src = "knakworst.png"
+})
