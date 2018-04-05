abacf624d5325744e8e038b877b28ad9ea071b5f
DeBasLightyear <baskleisen@gmail.com>
2018-04-05 16:10:09 +0200

    Cleaned up shop

--- js/script.js
+++ js/script.js
@@ -2,16 +2,16 @@
 
 const sausageLeft = document.getElementById("sausageLeft")
 sausageLeft.addEventListener("mouseover", ()=>{
-    sausageLeft.src = "knakworst_hover.png"
+    sausageLeft.src = "img/knakworst_hover.png"
 })
 sausageLeft.addEventListener("mouseout", ()=>{
-    sausageLeft.src = "knakworst.png"
+    sausageLeft.src = "img/knakworst.png"
 })
 
 const sausageRight = document.getElementById("sausageRight")
 sausageRight.addEventListener("mouseover", ()=>{
-    sausageRight.src = "knakworst_hover.png"
+    sausageRight.src = "img/knakworst_hover.png"
 })
 sausageRight.addEventListener("mouseout", ()=>{
-    sausageRight.src = "knakworst.png"
+    sausageRight.src = "img/knakworst.png"
 })
